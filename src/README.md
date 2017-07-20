4418f1958545465740c1d69d925dd80ecb8bbcc4
BrianTerry555 <brian@terryinvestments.com>
2017-07-19 23:38:02 -0600

    first repository

---
+++ src/README.md
@@ -0,0 +1,4 @@
+This is one of the first few apps that I made using React.
+Im getting better with the props and the passing process.
+
+Thanks for looking
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+//import components
+import ClickerContainer from "./containers/clicker-container.js";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>The Mighty Clicker</h1>
+        <ClickerContainer/>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
